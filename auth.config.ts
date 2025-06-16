13b4792c010417d54b0953d0bbd9e2d8dc91b0ba
johnnnray <johnrayvicanchuelo@gmail.com>
2025-06-16 17:31:44 +0900

    変更

--- auth.config.ts
+++ auth.config.ts
@@ -5,18 +5,6 @@ export const authConfig = {
     signIn: '/login',
   },
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.role = user.role;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token?.role) {
-        session.user.role = token.role;
-      }
-      return session;
-    },
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isAdmin = auth?.user?.role === 'admin'
